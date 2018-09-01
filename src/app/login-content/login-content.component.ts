fa83e323cb5634ebeca7b910cf65b03a32995428
Tamar Shore <tamarshore@gmail.com>
2018-09-01 17:35:40 +0300

    Added the login and main content components

---
+++ src/app/login-content/login-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-content',
+  templateUrl: './login-content.component.html',
+  styleUrls: ['./login-content.component.scss']
+})
+export class LoginContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
