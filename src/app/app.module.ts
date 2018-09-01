69f2532eaed91348e2c4cbffe9d5b1db1bf0d1dd
Tamar Shore <tamarshore@gmail.com>
2018-09-01 17:37:01 +0300

    Added the styles and logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import {MatButtonModule, MatIconModule, MatTabsModule, MatToolbarModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { LoginContentComponent } from './login-content/login-content.component';
+import { ContentComponent } from './content/content.component';
+import { LoginHeaderComponent } from './login-header/login-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginContentComponent,
+    ContentComponent,
+    LoginHeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    // Material modules
+    MatTabsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
