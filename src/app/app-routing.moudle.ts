ae3866369c88fffc4376481345e246da6238dfd0
Tamar Shore <tamarshore@gmail.com>
2018-09-10 14:18:35 +0300

    Fixed routing

--- src/app/app-routing.moudle.ts
+++ src/app/app-routing.moudle.ts
@@ -1,12 +1,45 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {MainComponent} from './main/main.component';
-import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {ContactComponent} from './contact/contact.component';
+import {SignUpComponent} from './sign-up/sign-up.component';
+import {MeasurementsComponent} from './measurements/measurements.component';
+import {BusinessComponent} from './business/business.component';
+import {AboutComponent} from './about/about.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MainComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'measurements',
+    component: MeasurementsComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent
+  },
+  {
+    path: 'business',
+    component: BusinessComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,35 @@ import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MainComponent } from './main/main.component';
 import {AppRoutingModule} from './app-routing.moudle';
-import {MainModule} from './main/main.module';
+import {MatButtonModule, MatIconModule, MatTabsModule, MatToolbarModule} from '@angular/material';
+import {HeaderComponent} from './header/header.component';
+import {LoginComponent} from './login/login.component';
+import {ContactComponent} from './contact/contact.component';
+import {SignUpComponent} from './sign-up/sign-up.component';
+import {MeasurementsComponent} from './measurements/measurements.component';
+import {BusinessComponent} from './business/business.component';
+import {AboutComponent} from './about/about.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MainModule
     // Material modules
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    AboutComponent,
+    MeasurementsComponent,
+    ContactComponent,
+    LoginComponent,
+    SignUpComponent,
+    BusinessComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
